28121b80cea7538b55facc37e6caaa419970a0b6
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2024-11-16 15:56:33 +0700

    commen

--- main.tf
+++ main.tf
@@ -238,178 +238,178 @@ resource "proxmox_virtual_environment_file" "cloud_config-sakinah" {
 # HOST
 # ====================
 # NASI UDUK
-resource "proxmox_virtual_environment_vm" "nasi-uduk-D05" {
-  name            = "nasi-uduk-D05"
-  node_name       = "its"
-  on_boot         = true
-  stop_on_destroy = true
-  scsi_hardware   = "virtio-scsi-single"
-  vm_id           = lookup(var.vm_id_list, "nasi-uduk-D05")
+# resource "proxmox_virtual_environment_vm" "nasi-uduk-D05" {
+#   name            = "nasi-uduk-D05"
+#   node_name       = "its"
+#   on_boot         = true
+#   stop_on_destroy = true
+#   scsi_hardware   = "virtio-scsi-single"
+#   vm_id           = lookup(var.vm_id_list, "nasi-uduk-D05")
 
-  clone {
-    datastore_id = "local-lvm"
-    node_name    = "its"
-    vm_id        = 5555
-  }
+#   clone {
+#     datastore_id = "local-lvm"
+#     node_name    = "its"
+#     vm_id        = 5555
+#   }
 
-  agent {
-    enabled = false
-  }
+#   agent {
+#     enabled = false
+#   }
 
-  initialization {
-    ip_config { #eth0
-      ipv4 {
-        address = lookup(var.ip_list, "nasi-uduk-D05")
-        gateway = lookup(var.gateaway_list, "nasi-uduk-D05")
-      }
-    }
-    user_account {
-      username = "ukashitam"
-      password = "hitamlegam"
-    }
-  }
+#   initialization {
+#     ip_config { #eth0
+#       ipv4 {
+#         address = lookup(var.ip_list, "nasi-uduk-D05")
+#         gateway = lookup(var.gateaway_list, "nasi-uduk-D05")
+#       }
+#     }
+#     user_account {
+#       username = "ukashitam"
+#       password = "hitamlegam"
+#     }
+#   }
 
-  cpu {
-    cores = 1
-    type  = "x86-64-v2-AES"
-  }
+#   cpu {
+#     cores = 1
+#     type  = "x86-64-v2-AES"
+#   }
 
-  memory {
-    dedicated = 1024 # 1GB
-    floating  = 1024
-  }
+#   memory {
+#     dedicated = 1024 # 1GB
+#     floating  = 1024
+#   }
 
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = "local:iso/focal-server-cloudimg-amd64.img"
-    file_format  = "raw"
-    interface    = "virtio0"
-    iothread     = true
-    size         = 3 # GB
-  }
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = "local:iso/focal-server-cloudimg-amd64.img"
+#     file_format  = "raw"
+#     interface    = "virtio0"
+#     iothread     = true
+#     size         = 3 # GB
+#   }
 
-  network_device {
-    enabled  = true
-    firewall = false
-    bridge   = "vmbr0"
-  }
-}
+#   network_device {
+#     enabled  = true
+#     firewall = false
+#     bridge   = "vmbr0"
+#   }
+# }
 
-# GEPREK
-resource "proxmox_virtual_environment_vm" "geprek-D05" {
-  name            = "geprek-D05"
-  node_name       = "its"
-  on_boot         = true
-  stop_on_destroy = true
-  scsi_hardware   = "virtio-scsi-single"
-  vm_id           = lookup(var.vm_id_list, "geprek-D05")
+# # GEPREK
+# resource "proxmox_virtual_environment_vm" "geprek-D05" {
+#   name            = "geprek-D05"
+#   node_name       = "its"
+#   on_boot         = true
+#   stop_on_destroy = true
+#   scsi_hardware   = "virtio-scsi-single"
+#   vm_id           = lookup(var.vm_id_list, "geprek-D05")
 
-  clone {
-    datastore_id = "local-lvm"
-    node_name    = "its"
-    vm_id        = 5555
-  }
+#   clone {
+#     datastore_id = "local-lvm"
+#     node_name    = "its"
+#     vm_id        = 5555
+#   }
 
-  agent {
-    enabled = false
-  }
+#   agent {
+#     enabled = false
+#   }
 
-  initialization {
-    ip_config { #eth0
-      ipv4 {
-        address = lookup(var.ip_list, "geprek-D05")
-        gateway = lookup(var.gateaway_list, "geprek-D05")
-      }
-    }
-    user_account {
-      username = "ukashitam"
-      password = "hitamlegam"
-    }
-  }
+#   initialization {
+#     ip_config { #eth0
+#       ipv4 {
+#         address = lookup(var.ip_list, "geprek-D05")
+#         gateway = lookup(var.gateaway_list, "geprek-D05")
+#       }
+#     }
+#     user_account {
+#       username = "ukashitam"
+#       password = "hitamlegam"
+#     }
+#   }
 
-  cpu {
-    cores = 1
-    type  = "x86-64-v2-AES"
-  }
+#   cpu {
+#     cores = 1
+#     type  = "x86-64-v2-AES"
+#   }
 
-  memory {
-    dedicated = 1024 # 1GB
-    floating  = 1024
-  }
+#   memory {
+#     dedicated = 1024 # 1GB
+#     floating  = 1024
+#   }
 
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = "local:iso/focal-server-cloudimg-amd64.img"
-    file_format  = "raw"
-    interface    = "virtio0"
-    iothread     = true
-    size         = 3 # GB
-  }
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = "local:iso/focal-server-cloudimg-amd64.img"
+#     file_format  = "raw"
+#     interface    = "virtio0"
+#     iothread     = true
+#     size         = 3 # GB
+#   }
 
-  network_device {
-    enabled  = true
-    firewall = false
-    bridge   = "vmbr0"
-  }
-}
+#   network_device {
+#     enabled  = true
+#     firewall = false
+#     bridge   = "vmbr0"
+#   }
+# }
 
-# KWETIAW
-resource "proxmox_virtual_environment_vm" "kwetiaw-D05" {
-  name            = "kwetiaw-D05"
-  node_name       = "its"
-  on_boot         = true
-  stop_on_destroy = true
-  scsi_hardware   = "virtio-scsi-single"
-  vm_id           = lookup(var.vm_id_list, "kwetiaw-D05")
+# # KWETIAW
+# resource "proxmox_virtual_environment_vm" "kwetiaw-D05" {
+#   name            = "kwetiaw-D05"
+#   node_name       = "its"
+#   on_boot         = true
+#   stop_on_destroy = true
+#   scsi_hardware   = "virtio-scsi-single"
+#   vm_id           = lookup(var.vm_id_list, "kwetiaw-D05")
 
-  clone {
-    datastore_id = "local-lvm"
-    node_name    = "its"
-    vm_id        = 5555
-  }
+#   clone {
+#     datastore_id = "local-lvm"
+#     node_name    = "its"
+#     vm_id        = 5555
+#   }
 
-  agent {
-    enabled = false
-  }
+#   agent {
+#     enabled = false
+#   }
 
-  initialization {
-    ip_config { #eth0
-      ipv4 {
-        address = lookup(var.ip_list, "kwetiaw-D05")
-        gateway = lookup(var.gateaway_list, "kwetiaw-D05")
-      }
-    }
-    user_account {
-      username = "ukashitam"
-      password = "hitamlegam"
-    }
-  }
+#   initialization {
+#     ip_config { #eth0
+#       ipv4 {
+#         address = lookup(var.ip_list, "kwetiaw-D05")
+#         gateway = lookup(var.gateaway_list, "kwetiaw-D05")
+#       }
+#     }
+#     user_account {
+#       username = "ukashitam"
+#       password = "hitamlegam"
+#     }
+#   }
 
-  cpu {
-    cores = 1
-    type  = "x86-64-v2-AES"
-  }
+#   cpu {
+#     cores = 1
+#     type  = "x86-64-v2-AES"
+#   }
 
-  memory {
-    dedicated = 1024 # 1GB
-    floating  = 1024
-  }
+#   memory {
+#     dedicated = 1024 # 1GB
+#     floating  = 1024
+#   }
 
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = "local:iso/focal-server-cloudimg-amd64.img"
-    file_format  = "raw"
-    interface    = "virtio0"
-    iothread     = true
-    size         = 3 # GB
-  }
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = "local:iso/focal-server-cloudimg-amd64.img"
+#     file_format  = "raw"
+#     interface    = "virtio0"
+#     iothread     = true
+#     size         = 3 # GB
+#   }
 
-  network_device {
-    enabled  = true
-    firewall = false
-    bridge   = "vmbr0"
-  }
-}
+#   network_device {
+#     enabled  = true
+#     firewall = false
+#     bridge   = "vmbr0"
+#   }
+# }
 
 # # TAHU TEK
 # resource "proxmox_virtual_environment_vm" "tahu-tek-D05" {
@@ -704,84 +704,84 @@ resource "proxmox_virtual_environment_vm" "kwetiaw-D05" {
 # ====================
 # ROUTER
 # ====================
-# ALFAMART 
-resource "proxmox_virtual_environment_vm" "alfamart-D05" {
-  name            = "alfamart-D05"
-  node_name       = "its"
-  on_boot         = true
-  stop_on_destroy = true
-  scsi_hardware   = "virtio-scsi-single"
-  vm_id           = lookup(var.vm_id_list, "alfamart-D05")
-  # hook_script_file_id = proxmox_virtual_environment_file.script1.id
+# # ALFAMART 
+# resource "proxmox_virtual_environment_vm" "alfamart-D05" {
+#   name            = "alfamart-D05"
+#   node_name       = "its"
+#   on_boot         = true
+#   stop_on_destroy = true
+#   scsi_hardware   = "virtio-scsi-single"
+#   vm_id           = lookup(var.vm_id_list, "alfamart-D05")
+#   # hook_script_file_id = proxmox_virtual_environment_file.script1.id
 
-  clone {
-    datastore_id = "local-lvm"
-    node_name    = "its"
-    vm_id        = 5555
-  }
+#   clone {
+#     datastore_id = "local-lvm"
+#     node_name    = "its"
+#     vm_id        = 5555
+#   }
 
-  agent {
-    enabled = true
-  }
+#   agent {
+#     enabled = true
+#   }
 
-  initialization {
-    ip_config { #eth0
-      ipv4 {
-        address = lookup(var.ip_list, "alfamart-D05-eth0")
-      }
-    }
-    ip_config { #eth1
-      ipv4 {
-        address = lookup(var.ip_list, "alfamart-D05-eth1")
-      }
-    }
-    ip_config { #eth2
-      ipv4 {
-        address = lookup(var.ip_list, "alfamart-D05-eth2")
-      }
-    }
-    # user_account {
-    #   username = "ukashitam"
-    #   password = "hitamlegam"
-    # }
-    user_data_file_id = proxmox_virtual_environment_file.cloud_config-alfamart.id
-  }
+#   initialization {
+#     ip_config { #eth0
+#       ipv4 {
+#         address = lookup(var.ip_list, "alfamart-D05-eth0")
+#       }
+#     }
+#     ip_config { #eth1
+#       ipv4 {
+#         address = lookup(var.ip_list, "alfamart-D05-eth1")
+#       }
+#     }
+#     ip_config { #eth2
+#       ipv4 {
+#         address = lookup(var.ip_list, "alfamart-D05-eth2")
+#       }
+#     }
+#     # user_account {
+#     #   username = "ukashitam"
+#     #   password = "hitamlegam"
+#     # }
+#     user_data_file_id = proxmox_virtual_environment_file.cloud_config-alfamart.id
+#   }
 
-  cpu {
-    cores = 1
-    type  = "x86-64-v2-AES"
-  }
+#   cpu {
+#     cores = 1
+#     type  = "x86-64-v2-AES"
+#   }
 
-  memory {
-    dedicated = 1024 # 1GB
-    floating  = 1024
-  }
+#   memory {
+#     dedicated = 1024 # 1GB
+#     floating  = 1024
+#   }
 
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = "local:iso/focal-server-cloudimg-amd64.img"
-    file_format  = "raw"
-    interface    = "virtio0"
-    iothread     = true
-    size         = 3 # GB
-  }
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = "local:iso/focal-server-cloudimg-amd64.img"
+#     file_format  = "raw"
+#     interface    = "virtio0"
+#     iothread     = true
+#     size         = 3 # GB
+#   }
 
-  network_device {
-    enabled  = true
-    firewall = false
-    bridge   = "vmbr0"
-  }
-  network_device {
-    enabled  = true
-    firewall = false
-    bridge   = "vmbr0"
-  }
-  network_device {
-    enabled  = true
-    firewall = false
-    bridge   = "vmbr0"
-  }
-}
+#   network_device {
+#     enabled  = true
+#     firewall = false
+#     bridge   = "vmbr0"
+#   }
+#   network_device {
+#     enabled  = true
+#     firewall = false
+#     bridge   = "vmbr0"
+#   }
+#   network_device {
+#     enabled  = true
+#     firewall = false
+#     bridge   = "vmbr0"
+#   }
+# }
 
 # # INDOMARET
 # resource "proxmox_virtual_environment_vm" "indomaret-D05" {
